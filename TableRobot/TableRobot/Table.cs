59507203e8b9a2383a6c5d2e0c6edac16f864719
Hamza Ijaz <hamza.ijaz980@gmail.com>
2023-03-04 13:50:25 +0500

    2. Added Table class and Validate method

---
+++ TableRobot/TableRobot/Table.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableRobot
+{
+    public class Table
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public Table(int rows, int cols) 
+        {
+            Rows = rows;
+            Columns = cols;
+        }
+
+        public bool IsValidPosition(int xCoordinate, int yCoordinate)
+        {
+            return xCoordinate >= 0 && xCoordinate <= Rows
+                && yCoordinate >= 0 && yCoordinate <= Columns;
+        }
+    }
+}
